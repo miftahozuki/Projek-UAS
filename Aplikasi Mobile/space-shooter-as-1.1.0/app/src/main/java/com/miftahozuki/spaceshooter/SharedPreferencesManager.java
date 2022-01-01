262e8d3b639d023ad4476fc25592e9cfd57ffcc2
Miftahus <miftahzk.payments@gmail.com>
2022-01-01 23:19:02 +0700

    semester 3

---
+++ Aplikasi Mobile/space-shooter-as-1.1.0/app/src/main/java/com/miftahozuki/spaceshooter/SharedPreferencesManager.java
@@ -0,0 +1,45 @@
+package com.miftahozuki.spaceshooter;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created on   : 8/12/2017
+ * Developed by : Hendrawan Adi Wijaya
+ * Github       : https://github.com/andevindo
+ * Website      : http://www.andevindo.com
+ */
+
+public class SharedPreferencesManager {
+
+    private String mName = "SpaceShooter";
+    private Context mContext;
+
+    public SharedPreferencesManager(Context context) {
+        mContext = context;
+    }
+
+    public void saveHighScore(int score, int meteorDestroyed, int enemyDestroyed){
+        SharedPreferences sp = mContext.getSharedPreferences(mName, Context.MODE_PRIVATE);
+        SharedPreferences.Editor e = sp.edit();
+        e.putInt("high_score", score);
+        e.putInt("meteor", meteorDestroyed);
+        e.putInt("enemy", enemyDestroyed);
+        e.commit();
+    }
+
+    public int getHighScore(){
+        SharedPreferences sp = mContext.getSharedPreferences(mName, Context.MODE_PRIVATE);
+        return sp.getInt("high_score", 0);
+    }
+
+    public int getMeteorDestroyed(){
+        SharedPreferences sp = mContext.getSharedPreferences(mName, Context.MODE_PRIVATE);
+        return sp.getInt("meteor", 0);
+    }
+
+    public int getEnemyDestroyed(){
+        SharedPreferences sp = mContext.getSharedPreferences(mName, Context.MODE_PRIVATE);
+        return sp.getInt("enemy", 0);
+    }
+}
